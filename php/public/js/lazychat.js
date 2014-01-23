f28e2905abc44974aa040421a7b40984711074f0
Juliano Amaral <contato@studyclass.com.br>
2014-01-23 01:43:30 -0200

    first beta draft with initial functions

--- php/public/js/lazychat.js
+++ php/public/js/lazychat.js
@@ -1,135 +1,89 @@
-jQuery(function($){    
-  moment.lang('pt-br');
-    
+;(function ( $, window, document, undefined ) {
+    var pluginName = "lazyChat";
+    var defaults = {
+        language: 'en',
+        title: 'LazyChat',
+    };
 
-  var conversationIdElem = $('.chat input[name=conversation-id]');
-  var dateConsultElem   = $('.chat input[name=date-consult]');
-  conversationIdElem.val('aa404c86b2aeb2b4981439cf3b1a3286ae9e88dc');
-  
-  /*$.post('/openchat/hash-start', function(data) {
-    conversationIdElem.val(data);
-    var conversationId = conversationIdElem.val();
-  });*/
-  
-  function update_timeline_scroll() {
-    var timelineElem         = $('.chat .message .timeline');
-    var timelineScrollTop    = timelineElem.scrollTop();
-    var timelineScrollHeight = timelineElem[0].scrollHeight;
-   
-    timelineElem.animate({
-      scrollTop: timelineScrollHeight
-    }, 500);
-  }
-  
-   $('.chat .header .toggle-chat').on('click', function() {
-    	var elem = $(this);
-     
-    	if (elem.text() == '-') {
-    		$('.chat .message').slideUp();
-    		elem.text('+');
-    	} else {
-    		$('.chat .message').slideDown();
-    		elem.text('-');
+    function Plugin ( element, options ) {
+      this.element   = element;
+      this.settings  = $.extend( {}, defaults, options );
+      this._defaults = defaults;
+      this._name     = pluginName;
+      this.init();
+      this.addEvents();
+    }
+
+    Plugin.prototype = {
+        init: function () {            
+            var currentLazyChatElem = $(this.element);
+
+            currentLazyChatElem.find('.lazychat-title').text(this.settings.title);
+        },
+        addEvents: function() {
+          var currentLazyChatElem = $(this.element);
+
+          //////////////////
+          // Toogle event //
+          //////////////////
           
-          	$('.chat .message .timeline').animate({
-              scrollTop: $('.chat .message .timeline')[0].scrollHeight
-            }, 500);
-    	}
-    });
-
-    $('.chat .input input').on('keypress', function(event) {      
-        var currentElem = $(this);
-        var message = currentElem.val();
-
-        if (event.keyCode == 13 && $.trim(message) != '') {
-          $('.chat .message .timeline').animate({
-            scrollTop: $('.chat .message .timeline')[0].scrollHeight
-          }, 500);
-          
-          $.post('/openchat/save-message', {conversation_id: conversationIdElem.val(), user_name: 'Juliano Amaral', message: message}, function(messageId) {            
-              if (messageId != '') {
-                  
-                var dateFromNow = moment().fromNow();
-                var appendHTML = '';
-        
-                appendHTML += '<div class="client" data-message-id="'+messageId+'">';
-                appendHTML += '<div class="row">';
-                appendHTML += '<div class="col-xs-12">';
-                appendHTML += '<span class="pull-left" style="width: 85%"><i>'+dateFromNow+'</i><br>';
-                appendHTML += '<div class="arrow"></div>';
-                appendHTML += message+'</span>';
-                appendHTML += '<div class="avatar pull-right" style="width: 13%">';
-                appendHTML += '<img src="http://beta.buscapremiada.com/portal/assets/img/tmp/business/chat.png" alt="">';
-                appendHTML += '</div>';
-                appendHTML += '</div>';
-                appendHTML += '</div>';
-              	appendHTML += '</div>';
+          currentLazyChatElem.find('.header .toggle-chat').on('click', function() {
+            var toogleChatElem = $(this);
+           
+            if (toogleChatElem.text() == '-') {
+              $('.lazychat .message').slideUp('fast');
+              toogleChatElem.text('+');
+            } else {
+              $('.lazychat .message').slideDown('fast');
+              toogleChatElem.text('-');
                 
-                $('.chat .message .timeline').append(appendHTML);
-            	currentElem.val('');
-              }
-            });
+              $('.lazychat .message .timeline').animate({
+                scrollTop: $('.lazychat .message .timeline')[0].scrollHeight
+              }, 500);
+            }
+          });
+
+          //////////////////////////
+          // Enter keypress event //
+          //////////////////////////
+
+          currentLazyChatElem.find('.input input').on('keypress', function(event) {
+            var inputMessageElem = $(this);
+            var message = inputMessageElem.val();
+
+            if (event.keyCode == 13 && $.trim(message) != '') {
+              currentLazyChatElem.find('.message .timeline').animate({
+                scrollTop: currentLazyChatElem.find('.message .timeline')[0].scrollHeight
+              }, 500);
+              
+              var appendHTML = '';
+      
+              appendHTML += '<div class="client" data-message-id="">';
+              appendHTML += '<div class="row">';
+              appendHTML += '<div class="col-xs-12">';
+              appendHTML += '<span class="pull-left" style="width: 85%"><i>Poucos segundos atrás</i><br>';
+              appendHTML += '<div class="arrow"></div>';
+              appendHTML += message+'</span>';
+              appendHTML += '<div class="avatar pull-right" style="width: 13%">';
+              appendHTML += '<img src="http://beta.buscapremiada.com/portal/assets/img/tmp/business/chat.png" alt="">';
+              appendHTML += '</div>';
+              appendHTML += '</div>';
+              appendHTML += '</div>';
+              appendHTML += '</div>';
+              
+              currentLazyChatElem.find('.message .timeline').append(appendHTML);
+              inputMessageElem.val('');
+          }
+        });
+      }
+    };
+
+    $.fn[ pluginName ] = function ( options ) {
+      return this.each(function() {
+        if ( !$.data( this, 'plugin_' + pluginName ) ) {
+            $.data( this, 'plugin_' + pluginName, new Plugin( this, options ) );
         }
-    });
-  
-    (function requestMessageLoop(){
-      	setTimeout(function() {
-        	$.ajax({
-              url: '/openchat/get-message',
-              type: 'POST',
-              data: {conversation_id: conversationIdElem.val(), date_consult: dateConsultElem.val()},
-              dataType: "html",
-              success: function (data) {
-                eval(data);
-                
-                var updateScroll = false;
-                
-                var timelineElem = $('.chat .message .timeline');
-                var scrollTop    = timelineElem.scrollTop();
-                var scrollHeight = timelineElem[0].scrollHeight - (timelineElem.outerHeight());
-                
-                if (scrollTop == scrollHeight) {
-                	updateScroll = true;
-                }
-                
-                for (var key in messagesArray) {            
-                  if ($('.chat .timeline').find('[data-message-id='+messagesArray[key]._id.$id+']').length <= 0) {
-                    var dateFromNow = moment(messagesArray[key].message.change_date).fromNow();
-                    var appendHTML = '';
-                      
-                    var appendHTML = '';
-                
-                    appendHTML += '<div class="client" data-message-id="'+messagesArray[key]._id.$id+'">';
-                    appendHTML += '<div class="row">';
-                    appendHTML += '<div class="col-xs-12">';
-                    appendHTML += '<span class="pull-left" style="width: 85%"><i>'+dateFromNow+'</i><br>';
-                    appendHTML += '<div class="arrow"></div>';
-                    appendHTML += messagesArray[key].message.message+'</span>';
-                    appendHTML += '<div class="avatar pull-right" style="width: 13%;">';
-                    appendHTML += '<img src="http://beta.buscapremiada.com/portal/assets/img/tmp/business/chat.png" alt="">';
-                    appendHTML += '</div>';
-                    appendHTML += '</div>';
-                    appendHTML += '</div>';
-                  	appendHTML += '</div>';
-                                    
-                    $('.chat .message .timeline').append(appendHTML);
-                  }
-                };
-                
-                var newScrollTop    = timelineElem.scrollTop();
-                var newScrollHeight = timelineElem[0].scrollHeight - (timelineElem.height() + 20);
-                
-                if (updateScroll && newScrollTop != newScrollHeight) {
-                  update_timeline_scroll();
-                }
-                
-                if (typeof newStorageDate != 'undefined') {
-                  dateConsultElem.val(newStorageDate);
-                }
-                
-                requestMessageLoop();
-              },
-            });
-        }, 500);
-      })();
-;});
+      });
+    };
+
+})( jQuery, window, document );
